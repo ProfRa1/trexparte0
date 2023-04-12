f86f1756f7875fed7f622df8c7e3843568089586
ProfRa1 <96995750+ProfRa1@users.noreply.github.com>
2023-04-12 11:44:16 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var trex, trex_running;
+function preload() {
+    trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+}
+
+function setup() {
+    createCanvas(600, 200);
+    trex = createSprite(50, 60, 20, 40);
+    trex.addAnimation("running", trex_running);
+}
+
+function draw() {
+    background("white");
+    drawSprites();
+}
